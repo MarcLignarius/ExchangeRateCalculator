239f5f6cd1c0348c0a83493dcda821bfb9ce31bf
Marc Davies <marcdaviesriot@gmail.com>
2020-07-09 21:16:31 -0700

    add variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const currencyOne = document.getElementById('currency-one');
+const currencyTwo = document.getElementById('currency-two');
+const amountOne = document.getElementById('amount-one');
+const amountTwo = document.getElementById('amount-two');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
