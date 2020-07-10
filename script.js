c9974d93ffdbbb68f10f2a317fffa2b219c27e23
Marc Davies <marcdaviesriot@gmail.com>
2020-07-09 21:18:45 -0700

    add event listeners

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const amountOne = document.getElementById('amount-one');
 const amountTwo = document.getElementById('amount-two');
 const rate = document.getElementById('rate');
 const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+  console.log('RAN');
+
+  // Event listeners
+  currencyOne.addEventListener('change', calculate);
+  amountOne.addEventListener('input', calculate);
+  currencyTwo.addEventListener('change', calculate);
+  amountTwo.addEventListener('input', calculate);
+}
+
+calculate();
